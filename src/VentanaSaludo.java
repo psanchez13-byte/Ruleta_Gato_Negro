3e945713c2aa097a19055e1b56c95de9c45230cb
Pedro Pablo Sanchez Nualart <p.sanchez13@ufromail.cl>
2026-09-09 22:25:56 -0300

    se inicia la base de la clase VentanaSaludo.java

--- src/VentanaSaludo.java
+++ src/VentanaSaludo.java
@@ -1,2 +1,8 @@
+import javax.swing.*;
+import java.awt.GridLayout;
+
 public class VentanaSaludo {
-}
+
+    private final JFrame frame = new JFrame("Bienvenido al Casino");
+    private final JLabel lblMensaje;
+    private final JButton btnJugar = new JButton("Ir a la Ruleta");
